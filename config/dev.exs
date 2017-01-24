37d7c3659a369dd9973cff78232428c91f0de634
maxpleaner <maxpleaner@gmail.com>
2017-01-23 22:25:56 -0800

    db config

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :server, Server.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "max",
+  password: "foo",
   database: "server_dev",
   hostname: "localhost",
   pool_size: 10
